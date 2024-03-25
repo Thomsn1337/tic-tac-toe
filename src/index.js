7f7aecee71b19019285b487c3208a591ee36dd8d
Thomas Steifler <steiflerthomas@gmail.com>
2024-03-25 17:55:27 +0100

    add function to play rounds

--- src/index.js
+++ src/index.js
@@ -7,22 +7,53 @@ const GameBoard = (function() {
             const tile = document.createElement("div");
             tile.classList.add("game-tile");
             tile.id = `tile-${i}`;
+
+            tile.addEventListener("click", () => {
+                Game.playRound(i);
+            });
+
             boardDiv.appendChild(tile);
+        }
+    }
+
+    function update(i, mark) {
+        const tile = document.querySelectorAll(".game-tile")[i];
+        if (board[i] === "" && tile.innerText === "") {
+            board[i] = mark;
+            tile.innerText = mark;
         }
     }
 
     return {
         draw,
+        update,
     };
 })();
 
+function createPlayer(name, mark) {
+    return { name, mark };
+}
+
 const Game = (function() {
+    const players = [
+        createPlayer("Player 1", "X"),
+        createPlayer("Player 2", "O"),
+    ];
+    let currentPlayer;
+
     function start() {
+        currentPlayer = 0;
         GameBoard.draw();
+    }
+
+    function playRound(tileIndex) {
+        GameBoard.update(tileIndex, players[currentPlayer].mark);
+        currentPlayer = currentPlayer === 0 ? 1 : 0;
     }
 
     return {
         start,
+        playRound,
     };
 })();
 
