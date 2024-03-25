7a726655b40c9d8cede1040078e9e32c62290cc5
Thomas Steifler <steiflerthomas@gmail.com>
2024-03-25 19:08:28 +0100

    add win condition logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const DisplayController = (function() {
+    function updateWinnerMessage(message) {
+        document.querySelector("#winner-message").innerText = message;
+    }
+
+    return {
+        updateWinnerMessage,
+    };
+})();
+
 const GameBoard = (function() {
     const board = ["", "", "", "", "", "", "", "", ""];
 
@@ -16,6 +26,8 @@ const GameBoard = (function() {
         }
     }
 
+    const getBoard = () => board;
+
     function update(i, mark) {
         const tile = document.querySelectorAll(".game-tile")[i];
         if (board[i] === "" && tile.innerText === "") {
@@ -27,6 +39,7 @@ const GameBoard = (function() {
     return {
         draw,
         update,
+        getBoard,
     };
 })();
 
@@ -40,15 +53,63 @@ const Game = (function() {
         createPlayer("Player 2", "O"),
     ];
     let currentPlayer;
+    let gameOver;
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
     function start() {
         currentPlayer = 0;
+        gameOver = false;
         GameBoard.draw();
     }
 
     function playRound(tileIndex) {
+        if (gameOver) {
+            return;
+        }
+
         GameBoard.update(tileIndex, players[currentPlayer].mark);
+
+        const gameStatus = checkGameStatus(GameBoard.getBoard());
+        if (gameStatus === "draw") {
+            gameOver = true;
+            DisplayController.updateWinnerMessage("It's a draw!");
+        } else if (gameStatus === "winner") {
+            gameOver = true;
+            DisplayController.updateWinnerMessage(
+                `${players[currentPlayer].name} has won!`,
+            );
+        }
+
         currentPlayer = currentPlayer === 0 ? 1 : 0;
+    }
+
+    function checkGameStatus(gameboard) {
+        if (gameboard.every((e) => e !== "")) {
+            return "draw";
+        }
+
+        for (let i = 0; i < winningConditions.length; i++) {
+            const [a, b, c] = winningConditions[i];
+            if (
+                gameboard[a] &&
+                gameboard[a] === gameboard[b] &&
+                gameboard[a] === gameboard[c]
+            ) {
+                return "winner";
+            }
+        }
+
+        return "continue";
     }
 
     return {
